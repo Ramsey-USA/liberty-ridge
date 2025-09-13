a00b4f3472c02d6261426541417f949ea0ec42b0
Matthew Jon Ramsey <mjramse1@email.asu.edu>
2025-09-13 20:23:40 +0000

    Update next.config.js output setting to 'standalone' and add comment for Firebase configuration clarity

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,6 +4,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
+// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD_MCkY8PxvwN9T2hqN6x9eknVCM8G4tT0",
